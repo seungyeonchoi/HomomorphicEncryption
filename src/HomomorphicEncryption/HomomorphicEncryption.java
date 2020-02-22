10003c9f438e0330bf110604fe3d3ac1dccb02e2
yeomsh <sanghee2911@gmail.com>
2020-02-23 00:40:05 +0900

    user, kgc, data 클래스 분리 x0의 기준 바꾸어야 함

--- src/HomomorphicEncryption/HomomorphicEncryption.java
+++ src/HomomorphicEncryption/HomomorphicEncryption.java
@@ -18,22 +18,22 @@ public class HomomorphicEncryption {
            User userB = new User(kgc.pkSet);
 //
 ////      data로 넘겨주는 pk는 생성자내에서 생성하는 것으로 변경
-//        d1 = new Data(userA,2,kgc.a,kgc.pkSet);
-//        d2 = new Data(userB,2,kgc.a,kgc.pkSet);
+        d1 = new Data(userA,new BigInteger("1234"),kgc.a,kgc.pkSet);
+        d2 = new Data(userB,new BigInteger("1234"),kgc.a,kgc.pkSet);
 //
-//        System.out.println(test());
+        System.out.println(test());
     }
 //
-//    public static Boolean test(){
-//        int parent = hash((d1.c1 % kgc.p )%kgc.a) *d2.c2;
-//        int child = hash((d2.c1 % kgc.p  )%kgc.a)*d1.c2;
-//        System.out.println(parent);
-//        System.out.println(child);
-//        return child/parent == 1 ? true : false;
-//    }
+    public static Boolean test(){
+        BigInteger parent = hash((d1.c1.mod(kgc.p).mod(kgc.a)).multiply(d2.c2));
+        BigInteger child = hash((d2.c1.mod(kgc.p).mod(kgc.a)).multiply(d1.c2));
+        System.out.println(parent);
+        System.out.println(child);
+        return child.divide(parent) == BigInteger.valueOf(1) ? true : false;
+    }
 
-    public static int hash(int exponent){ //data에도 있는데 하나로 하는 방법을 모르겠음!
-        return (int)Math.pow(2, exponent); //(의문)mod ? 를 해야할까
+    public static BigInteger hash(BigInteger exponent){ //data에도 있는데 하나로 하는 방법을 모르겠음!
+        return new BigInteger("2").pow(exponent.intValue()); //(의문)mod ? 를 해야할까
     }
 
 }
