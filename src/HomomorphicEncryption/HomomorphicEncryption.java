6254454134e826e70ef23edd1503e46118e0a4d9
yeomsh <sanghee2911@gmail.com>
2020-02-22 23:32:56 +0900

    kgc부분 bigInteger로 변경

--- src/HomomorphicEncryption/HomomorphicEncryption.java
+++ src/HomomorphicEncryption/HomomorphicEncryption.java
@@ -1,8 +1,6 @@
 package HomomorphicEncryption;
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Vector;
+import java.math.BigInteger;
 //정렬: command + alt+ l
 
 public class HomomorphicEncryption {
@@ -14,25 +12,25 @@ public class HomomorphicEncryption {
     public static void main(String[] args) {
         //0) parameter 세팅(현재는 디폴트) -> KGC
         //1) 공개키 쌍 만들기
-        kgc = new KGC(5);
+        kgc = new KGC(new BigInteger("5"));
         //2) user 만들기
-        User userA = new User(kgc.pkSet);
-        User userB = new User(kgc.pkSet);
-
-//      data로 넘겨주는 pk는 생성자내에서 생성하는 것으로 변경
-        d1 = new Data(userA,2,kgc.a,kgc.pkSet);
-        d2 = new Data(userB,2,kgc.a,kgc.pkSet);
-
-        System.out.println(test());
+//        User userA = new User(kgc.pkSet);
+//        User userB = new User(kgc.pkSet);
+//
+////      data로 넘겨주는 pk는 생성자내에서 생성하는 것으로 변경
+//        d1 = new Data(userA,2,kgc.a,kgc.pkSet);
+//        d2 = new Data(userB,2,kgc.a,kgc.pkSet);
+//
+//        System.out.println(test());
     }
-
-    public static Boolean test(){
-        int parent = hash((d1.c1 % kgc.p )%kgc.a) *d2.c2;
-        int child = hash((d2.c1 % kgc.p  )%kgc.a)*d1.c2;
-        System.out.println(parent);
-        System.out.println(child);
-        return child/parent == 1 ? true : false;
-    }
+//
+//    public static Boolean test(){
+//        int parent = hash((d1.c1 % kgc.p )%kgc.a) *d2.c2;
+//        int child = hash((d2.c1 % kgc.p  )%kgc.a)*d1.c2;
+//        System.out.println(parent);
+//        System.out.println(child);
+//        return child/parent == 1 ? true : false;
+//    }
 
     public static int hash(int exponent){ //data에도 있는데 하나로 하는 방법을 모르겠음!
         return (int)Math.pow(2, exponent); //(의문)mod ? 를 해야할까
--- src/HomomorphicEncryption/KGC.java
+++ src/HomomorphicEncryption/KGC.java
@@ -1,28 +1,39 @@
 package HomomorphicEncryption;
 
+import java.util.Random;
 import java.util.Vector;
 import java.util.Collections;
 import java.util.Comparator;
+import java.math.BigInteger;
 
+
+/* 20200222 수정사항
+ * w + riqid < a <p 해당 항목들 모두 다 biginteger
+ * 기존에서 변경된 부분
+ * 1. a,p,
+ * kgc부분 biginteger로 수정 및 계산 가능하게 변경
+ */
 public class KGC {
 
-    public static int pkSetSize = 5;
-    public static int lamda = 3;
-    public static int gamma = 15; //원래 조건 -> (int)(Math.random()*Math.pow(lamda,5));
-    public static int p = 327; //secret key (256 ~ 512)
-    public static int a = 13; //
-    public static Vector<Integer> pkSet = new Vector<>();
+    public static BigInteger pkSetSize = new BigInteger("5");
+    public static BigInteger lamda = new BigInteger("3");
+    public static BigInteger gamma = new BigInteger("15"); //원래 조건 -> (int)(Math.random()*Math.pow(lamda,5));
+    public static BigInteger p = new BigInteger("327");
+    public static BigInteger a = new BigInteger("13");
+    //public static int p = 327; //secret key (256 ~ 512)
+    //public static int a = 13; //
+    public static Vector<BigInteger> pkSet = new Vector<>();
 
     public KGC(){
         this(pkSetSize);
     }
 
-    public KGC(int pkSetSize){
+    public KGC(BigInteger pkSetSize){
         this.pkSetSize = pkSetSize;
         makePublicKeySet(this.pkSetSize);
     }
 
-    public KGC(int pkSetSize, int lamda, int gamma, int p, int a){
+    public KGC(BigInteger pkSetSize, BigInteger lamda, BigInteger gamma, BigInteger p, BigInteger a){
         this.pkSetSize = pkSetSize;
         this.lamda = lamda;
         this.gamma = gamma;
@@ -31,7 +42,7 @@ public class KGC {
         makePublicKeySet(this.pkSetSize);
     }
 
-    private void makePublicKeySet(int pkSetSize) {
+    private void makePublicKeySet(BigInteger pkSetSize) {
     /*
         Xi = p*qi + ri
         qi = 0 ~ (2^lamda)/2 // 0~ any integer
@@ -39,43 +50,58 @@ public class KGC {
          */
         pkSet.clear(); //pkSet 초기화
 
-        int [] qi = new int[pkSetSize];
-        int qMax = ((int)Math.pow(2,gamma))/p;
+        Random r = new Random();
 
-        for (int i = 0; i < pkSetSize; i++) {
-            qi[i] = (int)(Math.random()*qMax); // 0~ 2^(lamda)/p
+        Vector<BigInteger> qi = new Vector<>();
+        BigInteger qMax = new BigInteger("2").pow(lamda.pow(5).intValue()).divide(p);
+        System.out.println("qMax = " + qMax.toString());
+
+        for (int i = 0; i < pkSetSize.intValue(); i++) {
+            qi.add(new BigInteger(qMax.bitLength()-1,r));// 0~ 2^(lamda)/p
             for (int j = i-1; j >= 0; j--) { //중복제거
-                if (qi[j] == qi[i]) {
+                if (qi.get(j) == qi.get(i)) {
                     i--;
                     continue;
                 }
             }
-            System.out.println("qi: "+qi[i]);
+            System.out.println("qi: "+qi.get(i));
         }
 
-        int [] ri = new int[pkSetSize];
-        int rMax = 2*((int)Math.pow(2,lamda));
-        for (int i = 0; i < pkSetSize; i++) {
-            ri[i] = -((int)Math.pow(2,lamda))+ (int)(Math.random()*rMax); // 0~ 2^(lamda)/p
+        Vector<BigInteger> ri = new Vector<>();
+        BigInteger rMax = new BigInteger("2").pow(lamda.intValue()).multiply(BigInteger.valueOf(2));
+        System.out.println("rMax = " + rMax.toString());
+
+        for (int i = 0; i < pkSetSize.intValue(); i++) {
+            ri.add(new BigInteger("2").pow(lamda.intValue()).multiply(BigInteger.valueOf(-1))
+                    .add(new BigInteger(rMax.bitLength()-1,r)));
             for (int j = i-1; j >= 0; j--)  { //중복제거
-                if (ri[j] == ri[i]) {
+                if (ri.get(j) == ri.get(i)) {
                     i--;
                 }
             }
-            System.out.println("ri: "+ri[i]);
+            System.out.println("ri: "+ri.get(i));
         }
+//        int rMax = 2*((int)Math.pow(2,lamda));
+//        for (int i = 0; i < pkSetSize; i++) {
+//            ri[i] = -((int)Math.pow(2,lamda))+ (int)(Math.random()*rMax); // 0~ 2^(lamda)/p
+//            for (int j = i-1; j >= 0; j--)  { //중복제거
+//                if (ri[j] == ri[i]) {
+//                    i--;
+//                }
+//            }
+//                   }
 
 //        int [] qi = {1,2,2,32,46};
 //        int [] ri = {2,-2,1,1,13};
-        for(int i = 0; i<pkSetSize; i++){
-            pkSet.add(p*qi[i]+ri[i]);
+        for(int i = 0; i<pkSetSize.intValue(); i++){
+            pkSet.add(p.multiply(qi.get(i)).add(ri.get(i)));
         }
         Collections.sort(pkSet, Comparator.reverseOrder()); //X0 is the largest element
-        if (pkSet.get(0) % p != a){ //X0 mod p = a 조건 체크
-            int rest = p - (pkSet.get(0)%p - a);
-            int x0 = pkSet.get(0)+rest;
-            while (x0 % a == 0){ //x0 mod a =0 이면, x0 값 증가시킴 //근데 따져보니까 (p/a)*x =(k -1 ) 을 만족하는 x와 k가 있으면, 이 조건이 성립하는데 아마 그런경우가 많이 없을듯해 !
-                x0 += p;
+        if (pkSet.get(0).mod(p) != a){ //X0 mod p = a 조건 체크
+            BigInteger rest = p.subtract(pkSet.get(0).mod(p).subtract(a));
+            BigInteger x0 = pkSet.get(0).add(rest);
+            while (x0.mod(a) == BigInteger.valueOf(0)){ //x0 mod a =0 이면, x0 값 증가시킴 //근데 따져보니까 (p/a)*x =(k -1 ) 을 만족하는 x와 k가 있으면, 이 조건이 성립하는데 아마 그런경우가 많이 없을듯해 !
+                x0 = x0.add(p);
             }
             System.out.println(x0);
             pkSet.set(0,x0);
